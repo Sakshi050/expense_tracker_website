6fb6f73d723d6b3606bc88b36ba6a1abae2e8244
Sakshi050 <sakshi.583sahu@gmail.com>
2024-10-08 18:50:37 +0530

    added animation

--- frontend/src/styles/GlobalStyle.js
+++ frontend/src/styles/GlobalStyle.js
@@ -24,4 +24,29 @@ export const GlobalStyle = createGlobalStyle`
         font-size: clamp(1rem,1.5vw,1.2rem);
         overflow: hidden; 
     }
+    h1,h2,h3,h4,h5,h6{
+        color: var(--primary-color);
+    }
+    
+    .error{
+        color: red;
+        animation: shake 0.5s ease-in-out;
+        @keyframes shake {
+            0%{
+                transform: translateX(0);
+            }
+            25%{
+                transform: translateX(10px);
+            }
+            50%{
+                transform: translateX(-10px);
+            }
+            75%{
+                transform: translateX(10px);
+            }
+            100%{
+                transform: translateX(0);
+            }
+        }
+    }
 `;
