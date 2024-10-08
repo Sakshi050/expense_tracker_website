73c206121a2d2db69a95e4fd86c424690af85eb2
Sakshi050 <sakshi.583sahu@gmail.com>
2024-10-08 18:52:14 +0530

    incomes and expenses operations

--- frontend/src/context/globalContext.js
+++ frontend/src/context/globalContext.js
@@ -11,16 +11,93 @@ export const GlobalProvider = ({ children }) => {
     const [incomes, setIncomes] = useState([])
     const [expenses, setExpenses] = useState([])
     const [error, setError] = useState(null);
+
+    // calculate incomes
     const addIncome = async (income) => {
         const response = await axios.post(`${BASE_URL}add-income`, income)
             .catch((err) => {
                 setError(err.response.data.message)
             })
+        getIncomes()      // important to do this otherwise changes are triggered only when we refresh
     }
+
+    const getIncomes = async () => {
+        const response = await axios.get(`${BASE_URL}get-incomes`)
+        setIncomes(response.data)
+        // console.log(response.data)
+    }
+
+    const deleteIncome = async (id) => {
+        const res = await axios.delete(`${BASE_URL}delete-income/${id}`);
+        getIncomes();       // important to do this otherwise changes are triggered only when we refresh
+    }
+
+    const totalIncome = () => {
+        let totalIncome = 0;
+        incomes.forEach((income) => {
+            totalIncome += income.amount
+        })
+        return totalIncome;
+    }
+
+    // calculate expenses
+    const addExpense = async (income) => {
+        const response = await axios.post(`${BASE_URL}add-expense`, income)
+            .catch((err) => {
+                setError(err.response.data.message)
+            })
+        getExpenses()      // important to do this otherwise changes are triggered only when we refresh
+    }
+
+    const getExpenses = async () => {
+        const response = await axios.get(`${BASE_URL}get-expenses`)
+        setExpenses(response.data)
+        // console.log(response.data)
+    }
+
+    const deleteExpense = async (id) => {
+        const res = await axios.delete(`${BASE_URL}delete-expense/${id}`);
+        getExpenses();       // important to do this otherwise changes are triggered only when we refresh
+    }
+
+    const totalExpenses = () => {
+        let totalExpense = 0;
+        expenses.forEach((income) => {
+            totalExpense += income.amount
+        })
+        return totalExpense;
+    }
+
+    const totalBalance = () => {
+        return totalIncome() - totalExpenses()
+    }
+
+    const transactionHistory = () => {
+        const history = [...incomes, ...expenses]
+        history.sort((a, b) => {
+            return new Date(b.createdAt) - new Date(a.createdAt)
+        })
+        return history.slice(0, 3)
+    }
+
+
 
     return (
         <GlobalContext.Provider value={{
             addIncome,
+            getIncomes,
+            incomes,
+            deleteIncome,
+            totalIncome,
+            addExpense,
+            getExpenses,
+            expenses,
+            deleteExpense,
+            totalExpenses,
+            totalBalance,
+            transactionHistory,
+            error,
+            setError     //should be included for the error msg to disappear
         }}>
             {children}
         </GlobalContext.Provider>
